b04469fe725514fb78c9e394bc22fa145f7a8c19
Krithick <48274261+Krithick-Joy@users.noreply.github.com>
2021-10-10 22:33:59 +0530

    Update CreateItems.jsx

--- src/components/items/CreateItems.jsx
+++ src/components/items/CreateItems.jsx
@@ -66,29 +66,6 @@ export default function CreateItems() {
   const submitItemsData = async (e) => {
     e.preventDefault();
     updateErrorStatus({ status: 'inprogress' });
-    // let newFormFieldErrors = {};
-    // itemsFieldKeys.forEach(({ id, type: inputType }) => {
-    //   const value = itemsData[id];
-    //   if (!value) {
-    //     newFormFieldErrors = {
-    //       ...newFormFieldErrors,
-    //       [id]: `${id} is required`,
-    //     };
-    //   } else if (inputType === 'number' && isNaN(value)) {
-    //     newFormFieldErrors = {
-    //       ...newFormFieldErrors,
-    //       [id]: `${id} needs to be a number`,
-    //     };
-    //   } else if (inputType === 'email' && !validateEmail(value)) {
-    //     newFormFieldErrors = {
-    //       ...newFormFieldErrors,
-    //       [id]: `Enter a valid email`,
-    //     };
-    //   }
-    // });
-    // if (Object.keys(newFormFieldErrors).length > 0) {
-    //   updateFormFieldErrors(newFormFieldErrors);
-    // } else {
       const createStatus = await createItems(itemsData);
       if (createStatus.success) {
         updateErrorStatus({ status: 'success', message: successMsg });
